d522ee53bb97ee4fa62e41bacccf227860b0cb88
Qolbi <nzqadii@gmail.com>
2020-09-07 12:58:19 +0700

    v1.0
    Inisialisasi dan ubah path index.php

---
+++ ci4/app/Controllers/Kategori.php
@@ -0,0 +1,13 @@
+<?php namespace App\Controllers;
+
+class Kategori extends BaseController
+{
+	public function index()
+	{
+        // return view('welcome_message');
+        echo '<h1>Belajar ci4</h1>';
+	}
+
+	//--------------------------------------------------------------------
+
+}
